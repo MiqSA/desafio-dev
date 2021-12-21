67ff82530df17931a52e0b00567b740c67760ee8
MiqSA <eng.miqueias.sa@gmail.com>
2021-12-20 21:11:36 -0300

    Add files endpoint

---
+++ app/main/resources/files/files.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request, jsonify, send_from_directory
+from app.main.services.standards import Output
+from app.main.services.manage_files import CRUD
+from app.main.config import basedir
+import os
+
+
+files_blueprint = Blueprint('files', __name__)
+_basedir_files = basedir + '/databases/datafiles/'
+
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
+version = '/v1.0'
+exclusive_route = '/files'
+main_route = version + exclusive_route
+
+
+@files_blueprint.route(main_route + '/upload', methods=["POST"])
+def upload_file():
+    try:
+        file = request.files.get("myFile")
+        file.save(os.path.join(_basedir_files, file.filename))
+        if '.txt' not in file.filename:
+            return Output().response_api(403, 'Choose a valid file .txt')
+        CRUD(filename=f'{_basedir_files}/{file.filename}').main()
+        print('Success...')
+        return Output().response_api(200, 'Success upload file!!')
+    except Exception as err:
+        print('Error in  upload files', err)
+        return Output().response_api(500, None)
+
+
+
